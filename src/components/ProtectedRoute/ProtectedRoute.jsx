ef109c7fa98c43ba5dc14536181852131a062385
Edan Schwartz <edanschwartz@gmail.com>
2021-07-14 08:57:35 -0500

    Simplify ProtectedRoute code
    remove confusing authRedirect prop,
    replace with conditional rendering in App

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -13,48 +13,27 @@ import {useSelector} from 'react-redux';
 // by checking req.isAuthenticated for authentication
 // and by checking req.user for authorization
 
-function ProtectedRoute(props) {
+function ProtectedRoute({ component, children, ...props }) {
   const user = useSelector((store) => store.user);
 
-  // Using destructuring, this takes ComponentToProtect from component
-  // prop and grabs all other props to pass them along to Route
-  const {
-    // redirect path to be used if the user is authorized
-    authRedirect,
-    ...otherProps
-  } = props;
-
-  // Component may be passed in as as prop, or as a child
-  const ComponentToProtect = props.component || (() => props.children);
-
-  let ComponentToShow;
-
-  if (user.id) {
-    // if the user is logged in (only logged in users have ids)
-    // show the component that is protected
-    ComponentToShow = ComponentToProtect;
-  } else {
-    // if they are not logged in, check the loginMode on Redux State
-    // if the mode is 'login', show the LoginPage
-    ComponentToShow = LoginPage;
-  }
-
-
-  // redirect a logged in user if an authRedirect prop has been provided
-  if (user.id && authRedirect != null) {
-    return <Redirect exact from={otherProps.path} to={authRedirect} />;
-  } else if (!user.id && authRedirect != null) {
-    ComponentToShow = ComponentToProtect;
-  }
+  // Component may be passed in as a "component" prop,
+  // or as a child component.
+  const ProtectedComponent = component || (() => children);
 
   // We return a Route component that gets added to our list of routes
   return (
     <Route
       // all props like 'exact' and 'path' that were passed in
       // are now passed along to the 'Route' Component
-      {...otherProps}
+      {...props}
     >
-      <ComponentToShow />
+      {user.id ?
+        // If the user is logged in, show the protected component
+        <ProtectedComponent />
+        :
+        // Otherwise, redirect to the Loginpage
+        <LoginPage />
+      }
     </Route>
 
   );
