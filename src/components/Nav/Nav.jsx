5b2ea4624543611225a88a88342455dffb93008a
Matthew Black <matthewallenblack@gmail.com>
2021-12-20 11:07:52 -0600

    make 'Login / Register' link appear in nav when not logged in

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -14,12 +14,12 @@ function Nav() {
       </Link>
       <div>
         {/* If no user is logged in, show these links */}
-        {user.id === null &&
+        {!user.id && (
           // If there's no user, show login/registration links
           <Link className="navLink" to="/login">
             Login / Register
           </Link>
-        }
+        )}
 
         {/* If a user is logged in, show these links */}
         {user.id && (
