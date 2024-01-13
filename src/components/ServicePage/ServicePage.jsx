a03fd50036b18dc242f776646474dd345fc0702f
Braden Soller <braden.soller18@gmail.com>
2024-01-13 10:45:25 -0600

    added rest of services on service page

--- src/components/ServicePage/ServicePage.jsx
+++ src/components/ServicePage/ServicePage.jsx
@@ -4,18 +4,37 @@ import './service.css';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { useState } from 'react';
 import { Button } from '@mui/material';
+import { useEffect } from 'react';
 // This is one of our simplest components
 // It doesn't have local state
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is
 
+
+
+
 function ServicePage() {
   const history = useHistory()
   
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
 
   const MulchImages = [
     { url: "before-multch.jpg" },
     { url: "AFTER-ADD-MULTCH.jpg" }
+  ]
+
+  const TrimmingImages = [
+    {url: 'before-trimming.jpg'} ,
+    {url: 'After-trimming.jpg'}
+
+  ]
+
+  const WeedingImages = [
+    { url: 'before-weeding.png' },
+    { url: 'after-weeding.png' }
+
   ]
 
   const DeckImages = [
@@ -63,10 +82,21 @@ function ServicePage() {
         <p> text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet</p>
 
       </div>
+
+      <div className='TrimmingText'>
+        <h3>Tree/Bush Trimming</h3>
+        <p> text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet</p>
+
+      </div>
+      <div className='WeedingText'>
+        <h3>Weeding</h3>
+        <p> text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet text tet</p>
+
+      </div>
       <div className='MulchSlider'>
       <SimpleImageSlider
-        width={400}
-        height={300}
+        width={500}
+        height={400}
         images={MulchImages}
         showBullets={true}
         showNavs={true}
@@ -75,8 +105,8 @@ function ServicePage() {
       <Button variant='contained' color="warning" className='ScheduleService' onClick={goToAppointments}>schedule now</Button>
       <div className='DeckSlider'> 
         <SimpleImageSlider
-          width={400}
-          height={300}
+          width={500}
+          height={400}
           images={DeckImages}
           showBullets={true}
           showNavs={true}
@@ -87,21 +117,24 @@ function ServicePage() {
 
       <div className='RockSlider'>
         <SimpleImageSlider
-          width={400}
-          height={300}
+          width={500}
+          height={400}
           images={RockImages}
           showBullets={true}
           showNavs={true}
+       
+      
         />
 
       </div>
       <Button variant='contained' color="warning" className='RockButton' onClick={goToAppointments}>schedule now</Button>
 
-
+      <br />
+      <br />
       <div className='BrushSlider'>
         <SimpleImageSlider
-          width={400}
-          height={300}
+          width={500}
+          height={400}
           images={BrushImages}
           showBullets={true}
           showNavs={true}
@@ -110,6 +143,33 @@ function ServicePage() {
       </div>
       <Button variant='contained' color="warning" className='BrushButton' onClick={goToAppointments}>schedule now</Button>
 
+      <br />
+      <br />
+      <div className='TrimmingSlider'>
+        <SimpleImageSlider
+          width={500}
+          height={400}
+          images={TrimmingImages}
+          showBullets={true}
+          showNavs={true}
+        />
+
+      </div>
+      <Button variant='contained' color="warning" className='TrimmingButton' onClick={goToAppointments}>schedule now</Button>
+
+      <br />
+      <br />
+      <div className='WeedingSlider'>
+        <SimpleImageSlider
+          width={500}
+          height={400}
+          images={WeedingImages}
+          showBullets={true}
+          showNavs={true}
+        />
+
+      </div>
+      <Button variant='contained' color="warning" className='WeedingButton' onClick={goToAppointments}>schedule now</Button>
       </div>
    
 
