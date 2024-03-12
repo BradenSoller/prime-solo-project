580514ceebceaf63b72263b4a697e18a2f7d8eb2
Braden Soller <braden.soller18@gmail.com>
2024-03-11 19:47:57 -0500

    added "GET" query to DB file which allows to listen multiple services per appointment

--- database.sql
+++ database.sql
@@ -8,3 +8,27 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+        SELECT
+  a.id,
+  a.time_completed,
+  a.first_name,
+  a.email,
+  a.phone_number,
+  a.address,
+  a.zip,
+  a.description,
+  a.budget,
+ a.status,
+  json_agg(
+      json_build_object(
+          'service_id',
+          services.id,
+          'service_name',
+          services.name
+      )
+  ) services_array
+  FROM appointments a
+  LEFT JOIN appointments_services ac ON a.id = ac.appt_id
+  LEFT JOIN services ON ac.service_id = services.id
+GROUP BY a.id
+ORDER BY a.id;
