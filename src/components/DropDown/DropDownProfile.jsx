81bb3a9ae992be3d75ab796a72dc02ecdccffe68
Braden Soller <braden.soller18@gmail.com>
2024-12-05 09:59:24 -0600

    created start of drop down menu

---
+++ src/components/DropDown/DropDownProfile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+
+export function DropDownProfile() {
+    return (
+        <div className="Dropdown">
+        <ul>
+            <li>Log Out</li>
+            <li>MyAppointments</li>
+            </ul>
+        </div>
+)
+
+    
+
+
+
+
+
+
+
+}
